6e7c7ea57e4fcabc2fb9bfd7c591937e072f4675
germanmarquez56 <germanmarquez17@gmai.com>
2025-05-16 19:55:55 -0500

    16 de mayo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -22,6 +22,24 @@ def crud():
                 usuario.remove(diccionario)
                 break
     return render_template("crud.html", usuario=usuario)
+
+#ruta para editar la informacion de un usuario
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+    #TODO: capturar y buscar al usuario a editar
+    for diccionario in usuario: #para cada diccionario dentro de la lista evalue:
+        if diccionario['id']==id: 
+           usuario_a_editar=diccionario #hemos identificado los datos del usuario a editar
+           break
+
+    #TODO: actualizar la informacion del usuario seleccionado
+    if request.method=="POST":
+        usuario_a_editar["nombre"]=request.form.get("nombre") #el nombre nuevo sera el que llegue por un nuevo formulario
+        usuario_a_editar["correo"]=request.form.get("correo")
+
+        return redirect(url_for("crud")) #redireccione la aplicacion a la ruta de la funcion crud
+
+    return render_template('editar.html', usuario_a_editar=usuario_a_editar)
 
 
 
